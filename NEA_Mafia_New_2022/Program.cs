ef2265b15c3eab51e349046ad290eea560b53797
RealJeffBezos <48831476+jimber404@users.noreply.github.com>
2021-12-26 11:47:43 +0000

    Initial commit

---
+++ NEA_Mafia_New_2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NEA_Mafia_New_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
