791713ebb27269072a1deca160b359260dec8b29
RealJeffBezos <48831476+jimber404@users.noreply.github.com>
2021-12-27 12:27:11 +0000

    Implementing threading
    First try at implementing threading with listener/handler.

--- NEA_Mafia_New_2022/Server.cs
+++ NEA_Mafia_New_2022/Server.cs
@@ -1,3 +1,4 @@
+﻿
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
@@ -17,26 +18,52 @@ namespace NEA_Mafia_New_2022
 
             try
             {
-                string data = null;
-                byte[] bytes = null;
 
                 Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
                 listener.Bind(localEndPoint);
                 listener.Listen(10);
 
+/*                Console.WriteLine("Waiting for a connection...");
+                Socket handler = listener.Accept();
+                Console.WriteLine("Connected");*/
 
+                int counter = 0;
 
                 while (true)
                 {
+                    counter += 1;
                     Console.WriteLine("Waiting for a connection...");
                     Socket handler = listener.Accept();
                     Console.WriteLine("Connected");
-
                     handleClient client = new handleClient();
+                    client.startClient(handler, listener, counter.ToString());
                 }
 
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
+                /*while (true)
+                {
+                    bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+                    data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+                    handler.Send(msg);
+
+                    if (data.IndexOf("<EOF>") > -1)
+                    {
+
+                        Console.WriteLine("Connection with",
+                        listener.RemoteEndPoint.ToString(), "terminated");
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Text recived : {0}", data);
+                    }
+                }*/
+
+               /* handler.Shutdown(SocketShutdown.Both);
+                handler.Close();*/
             }
 
             catch (Exception e)
@@ -47,24 +74,61 @@ namespace NEA_Mafia_New_2022
             Console.WriteLine("\n press any key to continue...");
             Console.ReadKey();
         }
-
     }
 
     public class handleClient
     {
-        TcpClient clientSocket;
-        string clientNumber;
+        Socket handler, listener;
+        string clNo;
 
-        public void startClient(TcpClient inClientSocket, string clientNo)
+        public void startClient(Socket inClientHandler, Socket inClientListener ,string clientNo)
         {
-            this.clientSocket = inClientSocket;
-            this.clientNumber = clientNo;
+            this.handler = inClientHandler;
+            this.listener = inClientListener;
+            this.clNo = clientNo;
+
             Thread ctThread = new Thread(doChat);
+            ctThread.Start();
         }
 
         private void doChat()
         {
+            try
+            {
+                string data = null;
+                byte[] bytes = null;
 
+                while (true)
+                {
+                    bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+                    data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+                    handler.Send(msg);
+
+                    if (data.IndexOf("<EOF>") > -1)
+                    {
+
+                        Console.WriteLine("Connection with",
+                        listener.RemoteEndPoint.ToString(), "terminated");
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Text recived : {0}", data);
+                    }
+                }
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
