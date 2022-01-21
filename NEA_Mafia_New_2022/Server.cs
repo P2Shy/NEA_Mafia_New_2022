a83911bce5f5aa45d3ad152223f77800ef12f85e
RealJeffBezos <48831476+jimber404@users.noreply.github.com>
2022-01-21 11:13:06 +0000

    Overhaul of server with asynch socekts using streams

--- NEA_Mafia_New_2022/Server.cs
+++ NEA_Mafia_New_2022/Server.cs
@@ -10,155 +10,46 @@ namespace NEA_Mafia_New_2022
 {
     class Server
     {
+        private Socket __socket;
+        private byte[] __buffer = new byte[1024];
 
-        public static void StartServer()
+        public Server()
         {
-           
-            IPHostEntry host = Dns.GetHostEntry("localhost");
-            IPAddress ipAddress = host.AddressList[0];
-            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+            __socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
 
-            try
-            {
+        public void Bind(int port)
+        {
+            __socket.Bind(new IPEndPoint(IPAddress.Any, port));
+        }
 
-                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                listener.Bind(localEndPoint);
-                listener.Listen(30);
+        public void Listen(int backlog)
+        {
+            __socket.Listen(100);
+        }
 
-                int counter = 0;
-                int MaxPlayers = 2;
+        public void Accept()
+        {
+            __socket.BeginAccept(AcceptedCallback, null);
+        }
 
-                while (true)
-                {
-                    counter += 1;
-                    Console.WriteLine("Waiting for a connection...");
-                    Socket handler = listener.Accept();
-                    Console.WriteLine("Connected");
-                    HandleClient client = new HandleClient();
-                    client.StartClient(handler, listener, counter.ToString());
+        public void AcceptedCallback(IAsyncResult result)
+        {
+            Socket clientSocket = __socket.EndAccept(result);
+            Accept();
+            clientSocket.BeginReceive(__buffer, 0, __buffer.Length, SocketFlags.None, RecivedCallback, clientSocket);
+        }
 
-                    if (counter == MaxPlayers)
-                    {
-                        GameLogic.StartGame();
-                        handler.Send(Encoding.ASCII.GetBytes("<CMD> Start Game"));
-                    }
-                }
-            }
+        public void RecivedCallback(IAsyncResult result)
+        {
+            Socket clientSocket = result.AsyncState as Socket;
+            int bufferSize = clientSocket.EndReceive(result);
+            byte[] packet = new byte[bufferSize];
+            Array.Copy(__buffer, packet, packet.Length);
 
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-
-            Console.WriteLine("\n press any key to continue...");
-            Console.ReadKey();
+            __buffer = new byte[1024];
+            clientSocket.BeginReceive(__buffer, 0, __buffer.Length, SocketFlags.None, RecivedCallback, clientSocket);
         }
     }
-
-    public class HandleClient
-    {
-        public enum GameState { Waiting, Day, Night, Vote, Over };
-        GameState curState;
-
-        string[] ipList = new string[10];
-
-        Socket handler, listener;
-        string clNo;
-
-        public void StartClient(Socket inClientHandler, Socket inClientListener ,string clientNo)
-        {
-            this.handler = inClientHandler;
-            this.listener = inClientListener;
-            this.clNo = clientNo;
-
-            Thread ctThread = new Thread(DoChat);
-            ctThread.Start();
-
-            
-        }
-
-        private void Listner()
-        {
-            try
-            {
-                string data = null;
-                byte[] bytes = null;
-
-                while (true)
-                {
-                    bytes = new byte[1024];
-                    int bytesRec = handler.Receive(bytes);
-                    data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                    byte[] msg = Encoding.ASCII.GetBytes(data);
-                    handler.Send(msg);
-
-                    if (data.IndexOf("<EOF>") > -1)
-                    {
-
-                        Console.WriteLine("Connection with client", clNo, "terminated");
-                        break;
-                    }
-
-                    else if (data.IndexOf("<STR>") > -1)
-                    {
-
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("Text recived : {0}", data);
-                    }
-                }
-
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
-            }
-
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-        }
-
-        private void DoChat()
-        {
-
-            //Thread Game = new Thread(StartGame);
-
-            try
-            {
-                string data = null;
-                byte[] bytes = null;
-
-                while (true)
-                {
-                    bytes = new byte[1024];
-                    int bytesRec = handler.Receive(bytes);
-                    data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                    byte[] msg = Encoding.ASCII.GetBytes(data);
-                    handler.Send(msg);
-
-                     if (data.IndexOf("<EOF>") > -1)
-                    {
-
-                        Console.WriteLine("Connection with client",clNo,"terminated");
-                        break;
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("Text recived : {0}", data);
-                    }
-                }
-
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
-            }
-
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-        }
-    }
+        
 }
